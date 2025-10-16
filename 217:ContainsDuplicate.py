bd117189b913a78a2b344203ae3ba1c1c1dd8e1e
Owusu Kantankah <139204333+owsu@users.noreply.github.com>
2025-10-16 13:59:23 -0500

    Refactor comments in ContainsDuplicate.py
    Removed redundant comments and improved code clarity.

--- 217:ContainsDuplicate.py
+++ 217:ContainsDuplicate.py
@@ -1,3 +1,10 @@
+# Leetcode Easy Problem
+
+# Basically we make a set and we loop through the list adding it into the set. Now if it already exists in the set we know its a duplicate and we return true
+# if we loop through the whole list then there is no duplicates and we return False. Worse case scenario it loops through the entire list and creates an
+# entirely new one so this is O(n) time and space complexity. In terms of code lengthh I can definitely improve this code if I compare the list and set lengths.
+
+
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         hashset = set()
@@ -8,6 +15,3 @@ class Solution:
                 hashset.add(val)
         return False
 
-# Basically we make a set and we loop through the list adding it into the set. Now if it already exists in the set we know its a duplicate and we return true
-# if we loop through the whole list then there is no duplicates and we return False. Worse case scenario it loops through the entire list and creates an
-# entirely new one so this is O(n) time and space complexity. In terms of code lengthh I can definitely improve this code if I compare the list and set lengths.
