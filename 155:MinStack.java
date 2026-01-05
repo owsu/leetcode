c37ca9716a8e7c27d1037263d2fed43a3bf33c73
Owusu Kantankah <139204333+owsu@users.noreply.github.com>
2026-01-05 14:05:55 -0600

    Create MinStack class with min tracking
    Implement MinStack with push, pop, top, and getMin methods.

---
+++ 155:MinStack.java
@@ -0,0 +1,43 @@
+// Leetcode Medium
+
+class MinStack {
+    Deque<Integer> stack;
+    Deque<Integer> minStack;
+
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty()) {
+            minStack.push(val);
+        } else {
+            int pushval = (val > minStack.peek()) ? minStack.peek() : val;
+            minStack.push(pushval);
+        }
+    }
+    
+    public void pop() {
+        minStack.pop();
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
