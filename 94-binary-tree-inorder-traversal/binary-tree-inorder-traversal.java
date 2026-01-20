496a4fb014ced1985e74c894a018ad22b9a58606
Owusu Kantankah <139204333+owsu@users.noreply.github.com>
2026-01-20 14:25:27 -0600

    Time: 0 ms (100.00%) | Memory: 43.2 MB (38.42%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void searchTree(TreeNode node, List list) {
+        if (node == null) {return;}
+        searchTree(node.left, list);
+        list.add(node.val);
+        searchTree(node.right, list);
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> myList = new ArrayList<>();
+        searchTree(root, myList);
+        return myList;
+    }
+}
