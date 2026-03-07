3c3c032742cf0a2edcadca521d0fd65b765dae00
Owusu Kantankah <139204333+owsu@users.noreply.github.com>
2026-03-07 16:30:36 -0600

    Time: 6 ms (63.47%) | Memory: 43 MB (68.91%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int output = 0;
+        for (int center = 0; center < 2 * s.length() - 1; center++) {
+            int left = center / 2;
+            int right = left + center % 2;
+
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                output++;
+                left--;
+                right++;
+            }
+
+        }
+        return output;
+    }
+}
