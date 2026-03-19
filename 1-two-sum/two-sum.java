a933a85c69d8b28d6d81906612ed3242fd2eb6b2
Owusu Kantankah <139204333+owsu@users.noreply.github.com>
2026-03-18 20:18:45 -0500

    Time: 45 ms (26.98%) | Memory: 47.2 MB (34.75%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -3,10 +3,7 @@ class Solution {
      for (int i = 0; i < nums.length; i++) {
         for (int j = i+1; j < nums.length; j++) {
             if (nums[i] + nums[j] == target) {
-                int[] shit = new int[2];
-                shit[0] = i;
-                shit[1] = j;
-                return shit;
+                return new int[]{i, j};
             } 
         }
      }
