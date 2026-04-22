3e7ab97b3f6ecda79e953cd21623b70ef4016925
Owusu Kantankah <139204333+owsu@users.noreply.github.com>
2026-04-21 20:19:26 -0500

    Time: 15 ms (40.54%) | Memory: 31.2 MB (72.78%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        """
+        U - 
+        P - 
+        I - 
+        """
+        return len(nums) != len(set(nums))
